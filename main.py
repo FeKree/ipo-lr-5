0df50fa7da1005d3dd6cf7ebc8f2c8cd16c38bf2
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-10-22 12:37:39 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 vosem = input("Введите число в восьмеричной системе состоящее из 5 цифр: ")
 desyat = 0
-if len(vosem) == 5:
-    for i in range(5): #range(5)-кол-во символов 
-        num = int(vosem[i])
-        desyat += num * (8 ** (4 - i)) #вывод (8 ** (4 - i)) перевод с 8 в 10 систему
-    print("Число в десятичной системе:", desyat)
-elif len(vosem)<5:
-     print("Вы ввели менее 5 символов")
-else:
-    print("Вы ввели более 5 символов")
+for i in range(5): #range(5)-кол-во символов 
+    num = int(vosem[i])
+    desyat += num * (8 ** (4 - i)) #вывод (8 ** (4 - i)) перевод с 8 в 10 систему
+print("Число в десятичной системе:", desyat)
